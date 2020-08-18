ff4077b251d5e7150fc9f3cb72fe27bd3090bbdf
Justin Carruthers <jtcruthers@gmail.com>
2020-08-18 01:12:24 -0400

    Using state to update board

--- src/Board.vue
+++ src/Board.vue
@@ -18,15 +18,23 @@
 export default {
   name: 'Board',
   props: ["G", "ctx", "moves", "events", "playerID"],
-  data() {
-    return {
-      rows: [["", "", ""], ["", "", ""], ["", "", ""]],
-    }
-  },
   methods: {
     clickedCell(rowIndex, cellIndex) {
       const oneDimensionalIndex = rowIndex * 3 + cellIndex;
       this.$emit('cell-clicked', oneDimensionalIndex);
+    }
+  },
+  computed: {
+    rows() {
+      const { cells = Array(9).fill('') } = this.G;
+
+      const rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ].map(row => row.map(index => cells[index] || ''));
+      console.log(rows);
+      return rows;
     }
   },
 }
